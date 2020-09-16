ccf36349c003293db4391a596ce56ffeb58feb9b
Joshy <josh.ludahl@gmail.com>
2020-09-16 16:00:11 -0700

    init

---
+++ src/test/java/com/bulgae/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulgae.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
