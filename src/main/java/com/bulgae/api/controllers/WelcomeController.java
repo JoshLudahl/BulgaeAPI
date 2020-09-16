81216f6bd4cde513e6912a87749c0b6de8b1bd39
Joshy <josh.ludahl@gmail.com>
2020-09-16 16:20:12 -0700

    Added welcome controller

---
+++ src/main/java/com/bulgae/api/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.bulgae.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Generic greeting message";
+    }
+}
