355bcbfaa8a81fefba95d2b3b18713ac400e36b7
Joshy <josh.ludahl@gmail.com>
2020-09-18 11:33:01 -0700

    Testing CI

--- src/test/java/com/bulgae/api/ApiApplicationTests.java
+++ src/test/java/com/bulgae/api/ApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.bulgae.api;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ApiApplicationTests {
 
     @Test
     void contextLoads() {
+
+
+        String expectedMessage = "For input string";
+        String actualMessage = "exception.getMessage();";
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
 }
