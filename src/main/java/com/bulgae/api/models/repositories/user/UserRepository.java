4ac55223489c49c2df3590faea7e3f1a63ca55e8
Joshy <josh.ludahl@gmail.com>
2020-10-14 21:55:25 -0700

    connected and tested mongo

--- src/main/java/com/bulgae/api/models/repositories/user/UserRepository.java
+++ src/main/java/com/bulgae/api/models/repositories/user/UserRepository.java
@@ -2,12 +2,11 @@ package com.bulgae.api.models.repositories.user;
 
 import com.bulgae.api.models.users.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "user", path = "people")
 public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByLastName(@Param("name") String name);
+    List<User> findByEmail(String email);
 }
--- src/main/java/com/bulgae/api/models/users/User.java
+++ src/main/java/com/bulgae/api/models/users/User.java
@@ -5,16 +5,20 @@ import org.springframework.data.annotation.Id;
 public class User {
     private String firstName;
     private String lastName;
-    @Id private String id;
+    @Id
+    private String id;
     private String phone;
     private String extension;
     private String email;
     private Role role;
 
+    public User() {}
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-
+    }
+    public User(String email) {
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -23,5 +27,16 @@ public class User {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                '}';
     }
 }
