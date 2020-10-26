065727b8fe72b124ef3ebcae5dd7db238e9f0546
Joshy <josh.ludahl@gmail.com>
2020-10-25 21:18:51 -0700

    Tests

--- src/test/java/com/bulgae/api/controllers/WelcomeControllerTest.java
+++ src/test/java/com/bulgae/api/controllers/WelcomeControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -17,9 +16,6 @@ class WelcomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private WelcomeController welcomeController;
 
     @Test
     void status() throws Exception {
