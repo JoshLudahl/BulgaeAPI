c72d55fba09f887635b6062bc294557fd25b15e4
Joshy <josh.ludahl@gmail.com>
2020-10-26 19:11:48 -0700

    Tests

--- src/test/java/com/bulgae/api/controllers/WelcomeControllerTest.java
+++ src/test/java/com/bulgae/api/controllers/WelcomeControllerTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -19,8 +21,21 @@ class WelcomeControllerTest {
 
     @Test
     void status() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/status"))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void welcome() throws Exception {
+        MvcResult result = mockMvc
+                .perform(get("/"))
+                .andReturn();
+
+        String content = result
+                .getResponse()
+                .getContentAsString();
+
+        assertEquals(content, "Generic greeting message");
     }
 }
