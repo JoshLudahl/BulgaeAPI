96f5c4f4b87c348f102d8bf2403af3676d0bd234
Joshy <josh.ludahl@gmail.com>
2020-10-28 21:47:39 -0700

    Sign-up

--- src/main/java/com/bulgae/api/controllers/AuthenticateController.java
+++ src/main/java/com/bulgae/api/controllers/AuthenticateController.java
@@ -1,11 +1,20 @@
 package com.bulgae.api.controllers;
 
+import com.bulgae.api.models.repositories.users.UserRepository;
 import com.bulgae.api.models.users.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 public class AuthenticateController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/login")
     public User login() {
@@ -15,5 +24,23 @@ public class AuthenticateController {
     @RequestMapping("/logout")
     public int logout() {
         return 200;
+    }
+
+    @RequestMapping("/signup")
+    public Optional<User> signup(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam String email
+    ) {
+        User user = new User(firstName, lastName, email);
+        ArrayList<User> userFound;
+        userFound = userRepository.findByEmail(email);
+        if (userFound.size() == 0) {
+            userRepository.save(user);
+            return Optional.of(user);
+        } else {
+            // TODO: 10/28/20 Return something more useful
+            return null;
+        }
     }
 }
--- src/main/java/com/bulgae/api/models/repositories/users/UserRepository.java
+++ src/main/java/com/bulgae/api/models/repositories/users/UserRepository.java
@@ -4,6 +4,9 @@ import com.bulgae.api.models.users.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.ArrayList;
+
 @RepositoryRestResource
 public interface UserRepository extends MongoRepository<User, String> {
+    ArrayList<User> findByEmail(String email);
 }
